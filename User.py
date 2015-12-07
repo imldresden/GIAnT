fc670a95b71da4a4c7a59a42b50aaa843da7805a
David Stolze <s5300172@mail.zih.tu-dresden.de>
2015-12-07 04:46:36 +0100

    Update 1
    Former-commit-id: 8d24cbfa7298cddc390a92529b8058d1e2a3f0e1

--- User.py
+++ User.py
@@ -1,1 +1,34 @@
-__author__ = 'KillytheBid'
+import database
+
+users = []
+
+
+class User():
+    headPositions = []
+    viewpoints = []
+    touches = []
+    id = -1
+    color = "#FF0000"
+
+    def addHeadPosition(self, position):
+        self.headPositions.append(position)
+
+    def clearHeadPositions(self):
+        self.headPositions = []
+
+    def addViewpoint(self, point):
+        self.viewpoints.append(point)
+
+    def clearViewpoints(self):
+        self.viewpoints = []
+
+    def __init__(self, index, color):
+        self.id = index
+        self.color = color
+
+
+        self.headPositions = database.executeQry("SELECT x, y, z FROM normaltable where user = "+str(index)+" AND HEAD=1;", True)
+        self.touches = database.executeQry("SELECT x, y FROM normaltable where user = "+str(index)+" AND HEAD=0;", True)
+
+        users.append(self)
+
