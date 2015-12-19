5cd2e63806ce68f76320d6b26587f4614b36d77e
David Stolze <s5300172@mail.zih.tu-dresden.de>
2015-12-19 23:02:44 +0100

    Forgotten file
    Former-commit-id: 5423d5317d0c406b65d655efa1f092fc0543e857

---
+++ Variable_Width_Line.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from libavg import player, app, avg
+import math
+import Draw
+
+
+class Variable_Width_Line:
+    points = []
+    widths = []
+    color = 'FF0000'
+
+    def __init__(self, points, widths, color, parent):
+        # self.points = points
+        # self.widths = widths
+        self.points = points
+        self.widths = widths
+        self.color = color
+        self.__genGradient()
+
+        self.node = avg.MeshNode(parent=parent)
+        self.node.setBitmap(self.gradientBmp)
+        self.__genMesh()
+
+    def set_points_and_widths(self, points, widths):
+        self.points = points
+        self.widths = widths
+        self.__genGradient()
+        self.node.setBitmap(self.gradientBmp)
+        self.__genMesh()
+
+    def __genMesh(self):
+        vertexes = []
+        texcoords = [(0.1, 0), (0.1, 0)]
+        triangles = []
+        for i in range(len(self.points)):
+            p2 = self.points[i]
+            t2 = self.widths[i]
+            if (i < 1):
+                p1 = self.points[0]
+                t1 = self.widths[0]
+                p3 = self.points[i + 1]
+                t3 = self.widths[i + 1]
+            else:
+                p1 = self.points[i - 1]
+                t1 = self.widths[i - 1]
+                if (i >= len(self.points) - 1):
+                    t3 = self.widths[i]
+                    p3 = self.points[i]
+                else:
+                    t3 = self.widths[i + 1]
+                    p3 = self.points[i + 1]
+            linepos = Draw.calculate_line_intersection(p1, p2, p3, t1, t2, t3)
+            # width = math.sin(i / float(800)) * 10 + 20
+            vertexes.append(linepos[0])
+            vertexes.append(linepos[1])
+            texx = (i + 1) / float(len(self.widths) - 1)
+            texcoords.append((texx, 0))
+            texcoords.append((texx, 0))
+            triangles.append((i * 2, i * 2 + 1, i * 2 + 2))
+            triangles.append((i * 2 + 1, i * 2 + 2, i * 2 + 3))
+
+        vertexes.insert(0, vertexes[1])
+        vertexes.insert(0, vertexes[1])
+        self.node.vertexcoords = vertexes
+        self.node.texcoords = texcoords
+        self.node.triangles = triangles
+
+    def __genGradient(self):
+        canvas_id = str("gradient " + self.color)
+        if hasattr(self, 'canvas'):
+            self.canvas = player.deleteCanvas(canvas_id)
+        self.canvas = player.createCanvas(id=canvas_id, size=(len(self.widths) - 1, 1))
+        for x in range(len(self.widths)):
+            if self.widths[x] == 0:
+                opacity = 1
+            else:
+                opacity = 1 / (self.widths[x])
+            opacity += 0.03
+            avg.LineNode(pos1=(x + 0.5, -0.5), pos2=(x + 0.5, 1.5), color=self.color, opacity=opacity, parent=self.canvas.getRootNode())
+        self.canvas.render()
+        self.gradientBmp = self.canvas.screenshot()
