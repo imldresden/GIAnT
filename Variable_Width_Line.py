721fe75226ee7f0ea0261fee3c7ebf68182be862
David Stolze <s5300172@mail.zih.tu-dresden.de>
2015-12-19 23:06:11 +0100

    removed unused/broken imports
    Former-commit-id: b2e84dd486b50404bb04199086e316c040c70df0

--- Variable_Width_Line.py
+++ Variable_Width_Line.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from libavg import player, app, avg
-import math
 import Draw
 
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from libavg import app, avg, player
 from Draw import main_drawer
-import User
 
 
 app.App().run(main_drawer(), app_resolution='800x600')
