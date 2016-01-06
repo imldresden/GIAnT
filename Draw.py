387d69011414e7b0c7dfa2b034d63072905c92aa
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-06 21:39:02 +0100

    Update for code review
    Former-commit-id: 27ee3bd671820fd7d162dd127e2192b41457ab2a

--- Draw.py
+++ Draw.py
@@ -1,10 +1,10 @@
 from libavg import app, avg
 import libavg
-import axis
 import User
 import Util
+import axis
 import Visualization
-import database
+import global_values
 import time
 import math
 
@@ -14,7 +14,7 @@ class main_drawer(app.MainDiv):
     visualizations = []
     resolution = (1920, 1000)
     viewport_change_duration = 0.3
-
+    '''
     zoom_target = 1
     zoom_current = 1
     zoom_last = 1
@@ -27,6 +27,7 @@ class main_drawer(app.MainDiv):
     shift_last = 0.5
     shift_change_progress = 0
     shift_change_starttime = 0
+    '''
 
     def onInit(self):
         # self.resolution = libavg.app.instance._resolution
@@ -39,50 +40,27 @@ class main_drawer(app.MainDiv):
         app.keyboardmanager.bindKeyDown(keyname='Right', handler=self.shift_forward)
         app.keyboardmanager.bindKeyDown(keyname='Left', handler=self.shift_back)
 
+
+
         # axes
         self.x_axis = axis.AxisNode(size=(self.width - 100, 50), pos=(50, self.height - 100), parent=self,
-                                    data_range=(database.min_time, database.max_time),
+                                    data_range=global_values.total_range,
                                     unit="ms")
         self.time_axis = axis.TimeAxisNode(size=(self.width - 100, 50), pos=(50, self.height - 50), parent=self,
-                                           data_range=(database.min_time, database.max_time),
+                                           data_range=global_values.total_range,
                                            unit="ms")
         self.y_axis = axis.AxisNode(size=(libavg.app.instance._resolution[1] - 100, 50), pos=(0, 0), parent=self, vertical=True,
-                                    data_range=(database.min_x, database.max_x))
+                                    data_range=global_values.total_range)
 
     def onFrame(self):
-        # print 1 / (time.time() - self.last_time)  # FPS
-        self.last_time = time.time()
-        if self.zoom_change_starttime != 0 or self.shift_change_starttime != 0:
-            if self.zoom_change_starttime != 0:
-                if self.zoom_change_progress == 1:
-                    self.zoom_last = self.zoom_target
-                    self.zoom_change_starttime = 0
-                    return
-
-                self.zoom_change_progress = (time.time() - self.zoom_change_starttime) / self.viewport_change_duration
-                self.zoom_change_progress = min(1, self.zoom_change_progress)
-
-                self.zoom_current = self.zoom_last * (1 - self.zoom_change_progress) + self.zoom_target * self.zoom_change_progress
-
-            if self.shift_change_starttime != 0:
-                if self.shift_change_progress == 1:
-                    self.shift_last = self.shift_target
-                    self.shift_change_starttime = 0
-                    return
-
-                self.shift_change_progress = (time.time() - self.shift_change_starttime) / self.viewport_change_duration
-                self.shift_change_progress = min(1, self.shift_change_progress)
-
-                self.shift_current = self.shift_last * (1 - self.shift_change_progress) + self.shift_target * self.shift_change_progress
+        if global_values.update_interval_range():
             for visualization in self.visualizations:
-                visualization.start = (self.zoom_current - 1) * self.shift_current
-                visualization.end = 1 - (self.zoom_current - 1) * (1 - self.shift_current)
                 visualization.createLine()
 
-            true_range = database.max_time - database.min_time
-            offset = self.shift_current * true_range
+            true_range = global_values.total_range[1] - global_values.total_range[0]
+            offset = global_values.get_interval_range()[0]  # self.shift_current * true_range
             self.x_axis.update(visualization.start * true_range, visualization.end * true_range, offset)
-            self.time_axis.update(database.min_time, database.max_time, visualization.start * true_range, visualization.end * true_range)
+            self.time_axis.update(global_values.total_range[0], global_values.total_range[1], visualization.start * true_range, visualization.end * true_range)
 
     def draw_line(self, p1, p2, color, thickness, last_thickness, opacity):
         return libavg.LineNode(pos1=p1, pos2=p2, color=color, strokewidth=thickness, parent=self)
@@ -95,47 +73,16 @@ class main_drawer(app.MainDiv):
         return [start_points[0], end_points[0], end_points[1], start_points[1]]
 
     def shift_back(self):
-        try:
-            self.shift_last = self.shift_current
-            self.shift_target = self.shift_target - pow(2 - self.zoom_target, 1.5)
-            self.shift_target = min(1, max(0, self.shift_target))
-            self.shift_change_starttime = time.time()
-            self.shift_change_progress = 0
-        except:
-            None
+        global_values.shift_time(False)
 
     def shift_forward(self):
-        try:
-            self.shift_last = self.shift_current
-            self.shift_target = self.shift_target + pow(2 - self.zoom_target, 1.5)
-            self.shift_target = min(1, max(0, self.shift_target))
-            self.shift_change_starttime = time.time()
-            self.shift_change_progress = 0
-        except:
-            None
+        global_values.shift_time(True)
 
     def onMouseWheel(self, event):
         if event.motion.y > 0:
-            self.zoomIn()
+            global_values.zoom_in_at(event.pos[0] / self.resolution[0])
         else:
-            self.zoomOut()
-
-    def zoomIn(self):
-        self.zoom_last = self.zoom_current
-        # self.zoom_target += (1.5 - self.zoom_target) * self.zoom_amount
-        self.zoom_target += (2 - self.zoom_target) / float(5)
-        self.zoom_target = min(2, max(1, self.zoom_target))
-        self.zoom_change_starttime = time.time()
-        self.zoom_change_progress = 0
-
-    def zoomOut(self):
-        self.zoom_last = self.zoom_current
-        strength = (1.5 - self.zoom_target) * self.zoom_amount
-        # self.zoom_target -= self.zoom_target / ((1 / strength) + 1)
-        self.zoom_target -= (2 - self.zoom_target) / float(4)
-        self.zoom_target = min(2, max(1, self.zoom_target))
-        self.zoom_change_starttime = time.time()
-        self.zoom_change_progress = 0
+            global_values.zoom_out_at(event.pos[0] / self.resolution[0])
 
 
 def calculate_line_intersection(p1, p2_selected, p3, thickness1, thickness2_selected, thickness3):
@@ -150,27 +97,26 @@ def calculate_line_intersection(p1, p2_selected, p3, thickness1, thickness2_sele
         normalized_vector_1 = (vector_1[0] / vector_length_1, vector_1[1] / vector_length_1)
         normalized_vector_2 = (vector_2[0] / vector_length_2, vector_2[1] / vector_length_2)
     except:
-        normalized_vector_1 = (0,1)
-        normalized_vector_2 = (0,1)
+        normalized_vector_1 = (0, 1)
+        normalized_vector_2 = (0, 1)
 
     left_1 = (p1[0] - normalized_vector_1[1] * thickness1, p1[1] + normalized_vector_1[0] * thickness1)
     left2_1 = (p2_selected[0] - normalized_vector_1[1] * thickness2_selected, p2_selected[1] + normalized_vector_1[0] * thickness2_selected)
     left2_2 = (p2_selected[0] - normalized_vector_2[1] * thickness2_selected, p2_selected[1] + normalized_vector_2[0] * thickness2_selected)
     left_3 = (p3[0] - normalized_vector_2[1] * thickness3, p3[1] + normalized_vector_2[0] * thickness3)
 
-    vector_left_1 = (left2_1[0] - left_1[0], left2_1[1] - left_1[1])
-    vector_left_2 = (left2_2[0] - left_3[0], left2_2[1] - left_3[1])
-
     right_1 = (p1[0] + normalized_vector_1[1] * thickness1, p1[1] - normalized_vector_1[0] * thickness1)
     right2_1 = (p2_selected[0] + normalized_vector_1[1] * thickness2_selected, p2_selected[1] - normalized_vector_1[0] * thickness2_selected)
     right2_2 = (p2_selected[0] + normalized_vector_2[1] * thickness2_selected, p2_selected[1] - normalized_vector_2[0] * thickness2_selected)
     right_3 = (p3[0] + normalized_vector_2[1] * thickness3, p3[1] - normalized_vector_2[0] * thickness3)
 
-    vector_right_1 = (right2_1[0] - right_1[0], right2_1[1] - right_1[1])
-    vector_right_2 = (right2_2[0] - right_3[0], right2_2[1] - right_3[1])
-
     intersection_point_1 = Util.line_intersection((left_1, left2_1), (left2_2, left_3))
     intersection_point_2 = Util.line_intersection((right_1, right2_1), (right2_2, right_3))
+    distance_1 = Util.get_length((intersection_point_1[0] - tuple(p2_selected)[0], intersection_point_1[1] - tuple(p2_selected)[1]))
+
+    if (distance_1 > 12):
+        return [left2_2, right2_2]
+
     return [intersection_point_1, intersection_point_2]
 
 
