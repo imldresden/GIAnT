017a9ac86ea4b58fc2fc67b2c7cb9d4c1fae1712
Gustav Hahn <s3380858@mail.zih.tu-dresden.de>
2016-01-19 21:28:27 +0100

    implemented own custom SliderBase and ScrollBar from libavg used as interval slider scrollbar of current interval is draggable, but visualization is not responding properly yet (waiting for implementation of global publisher)
    Former-commit-id: 17123c79b2c1722a1b7a42b31ec0a1a3183cada2

--- custom_slider.py
+++ custom_slider.py
@@ -1,5 +1,159 @@
+import math
 import libavg
-from libavg import widget, avg
+from libavg import widget, avg, gesture
+
+
+class IntervalSliderBase(avg.DivNode):
+    """
+    Curstom SliderBase to modify slider track (don't even create one).
+    """
+    THUMB_POS_CHANGED = avg.Publisher.genMessageID()
+    PRESSED = avg.Publisher.genMessageID()
+    RELEASED = avg.Publisher.genMessageID()
+
+    def __init__(self, cfg, width=0, range=(0., 1.), thumbPos=0.0, parent=None, **kwargs):
+        super(IntervalSliderBase, self).__init__(**kwargs)
+        self.registerInstance(self, parent)
+
+        self.publish(IntervalSliderBase.THUMB_POS_CHANGED)
+        self.publish(IntervalSliderBase.PRESSED)
+        self.publish(IntervalSliderBase.RELEASED)
+
+        self._initThumb(cfg)
+
+        self._range = range
+        self._thumbPos = thumbPos
+
+        self.subscribe(self.SIZE_CHANGED, lambda newSize: self._positionNodes())
+        self.size = (width, 10)
+
+        self.__recognizer = gesture.DragRecognizer(self._thumbNode, friction=-1, detectedHandler=self.__onDragStart,
+                                                   moveHandler=self.__onDrag, upHandler=self.__onUp)
+
+    def getRange(self):
+        return self._range
+
+    def setRange(self, range):
+        self._range = (float(range[0]), float(range[1]))
+        self._positionNodes()
+
+    # range[1] > range[0]: Reversed scrollbar.
+    range = property(getRange, setRange)
+
+    def getThumbPos(self):
+        return self._thumbPos
+
+    def setThumbPos(self, thumbPos):
+        self._positionNodes(thumbPos)
+
+    thumbPos = property(getThumbPos, setThumbPos)
+
+    def _positionNodes(self, newSliderPos=None):
+        if newSliderPos is not None:
+            self._thumbPos = float(newSliderPos)
+
+        self._constrainSliderPos()
+
+        pixelRange = self._getScrollRangeInPixels()
+        if self._getSliderRange() == 0:
+            thumbPixelPos = 0
+        else:
+            thumbPixelPos = (((self._thumbPos-self._range[0])/self._getSliderRange())*pixelRange)
+        self._thumbNode.x = thumbPixelPos
+
+    def __onDragStart(self):
+        self._thumbNode.visibleid = "DOWN"
+        self.__dragStartPos = self._thumbPos
+        self.notifySubscribers(IntervalScrollBar.PRESSED, [])
+
+    def __onDrag(self, offset):
+        pixelRange = self._getScrollRangeInPixels()
+        if pixelRange == 0:
+            normalizedOffset = 0
+        else:
+            normalizedOffset = offset.x/pixelRange
+        oldThumbPos = self._thumbPos
+        self._positionNodes(self.__dragStartPos + normalizedOffset*self._getSliderRange())
+        if self._thumbPos != oldThumbPos:
+            self.notifySubscribers(IntervalScrollBar.THUMB_POS_CHANGED, [self._thumbPos])
+
+    def __onUp(self, offset):
+        self.__onDrag(offset)
+        self._thumbNode.visibleid = "UP"
+        self.notifySubscribers(IntervalScrollBar.RELEASED, [])
+
+
+class IntervalScrollBar(IntervalSliderBase):
+    """
+    Custom ScrollBar as interval slider.
+    """
+    def __init__(self, thumbExtent=0.1, skinObj=widget.skin.Skin.default, **kwargs):
+        self.__thumbExtent = thumbExtent
+        cfg = skinObj.defaultSliderCfg["horizontal"]
+        super(IntervalScrollBar, self).__init__(cfg=cfg, **kwargs)
+
+    def _initThumb(self, cfg):
+        thumbUpBmp = avg.ImageNode(href="images/quad_white.png", size=(1024, 1024)).getBitmap()
+        thumbDownBmp = avg.ImageNode(href="images/quad_white.png", size=(1024, 1024)).getBitmap()
+        thumbDisabledBmp = avg.ImageNode(href="images/quad_white.png", size=(1024, 1024)).getBitmap()
+        endsExtent = 1
+
+        self._thumbNode = IntervalScrollBarThumb(upBmp=thumbUpBmp, downBmp=thumbDownBmp,
+                                                 disabledBmp=thumbDisabledBmp, endsExtent=endsExtent)
+        self.appendChild(self._thumbNode)
+
+    def getThumbExtent(self):
+        return self.__thumbExtent
+
+    def setThumbExtent(self, thumbExtent):
+        self.__thumbExtent = float(thumbExtent)
+        self._positionNodes()
+
+    thumbExtent = property(getThumbExtent, setThumbExtent)
+
+    def _getScrollRangeInPixels(self):
+        return self.size.x - self._thumbNode.width
+
+    def _positionNodes(self, newSliderPos=None):
+        effectiveRange = math.fabs(self._range[1] - self._range[0])
+        thumbExtent = (self.__thumbExtent/effectiveRange)*self.size.x
+        self._thumbNode.width = thumbExtent
+        super(IntervalScrollBar, self)._positionNodes(newSliderPos)
+        if self._range[1] < self._range[0]:
+            # Reversed (upside-down) scrollbar
+            self._thumbNode.x -= thumbExtent
+
+    def _getSliderRange(self):
+        if self._range[1] > self._range[0]:
+            return self._range[1] - self._range[0] - self.__thumbExtent
+        else:
+            return self._range[1] - self._range[0] + self.__thumbExtent
+
+    def _constrainSliderPos(self):
+        rangeMin = min(self._range[0], self._range[1])
+        rangeMax = max(self._range[0], self._range[1])
+        self._thumbPos = max(rangeMin, self._thumbPos)
+        self._thumbPos = min(rangeMax-self.__thumbExtent, self._thumbPos)
+
+
+class IntervalScrollBarThumb(widget.SwitchNode):
+    """
+    Custom ScrollBar thumb.
+    """
+    def __init__(self, upBmp, downBmp, disabledBmp, endsExtent, minExtent=-1, **kwargs):
+
+        super(IntervalScrollBarThumb, self).__init__(nodeMap=None, **kwargs)
+
+        self.__upNode = widget.HStretchNode(src=upBmp, endsExtent=endsExtent, minExtent=minExtent)
+        self.__downNode = widget.HStretchNode(src=downBmp, endsExtent=endsExtent, minExtent=minExtent)
+        self.__disabledNode = widget.HStretchNode(src=disabledBmp, endsExtent=endsExtent, minExtent=minExtent)
+
+        self.setNodeMap({
+            "UP": self.__upNode,
+            "DOWN": self.__downNode,
+            "DISABLED": self.__disabledNode
+        })
+        self.visibleid = "UP"
 
 
 class IntervalSlider(widget.Slider):
@@ -28,44 +182,3 @@ class IntervalSliderThumb(widget.SwitchNode):
     def __init__(self, upNode, downNode, disabledNode, **kwargs):
         nodeMap = {"UP": upNode, "DOWN": downNode, "DISABLED": disabledNode}
         super(IntervalSliderThumb, self).__init__(nodeMap=nodeMap, visibleid="UP", **kwargs)
-
-
-class IntervalScrollBar(widget.ScrollBar):
-    """
-    Custom ScrollBar as interval slider.
-    """
-    def __init__(self, thumbExtent=0.1, **kwargs):
-        self.__thumbExtent = thumbExtent
-        super(IntervalScrollBar, self).__init__(**kwargs)
-
-    def _initThumb(self, cfg):
-        thumbUpBmp = avg.ImageNode(href="images/hor_grey.png", size=(1024, 1024)).getBitmap()
-        thumbDownBmp = avg.ImageNode(href="images/hor_grey.png", size=(1024, 1024)).getBitmap()
-        thumbDisabledBmp = avg.ImageNode(href="images/hor_grey.png", size=(1024, 1024)).getBitmap()
-        endsExtent = cfg["thumbEndsExtent"]
-
-        print "{}".format(type(thumbUpBmp))
-
-        self._thumbNode = IntervalScrollBarThumb(upBmp=thumbUpBmp, downBmp=thumbDownBmp,
-                                                 disabledBmp=thumbDisabledBmp, endsExtent=endsExtent)
-        self.appendChild(self._thumbNode)
-
-
-class IntervalScrollBarThumb(widget.SwitchNode):
-    """
-    Custom ScrollBar thumb.
-    """
-    def __init__(self, upBmp, downBmp, disabledBmp, endsExtent, minExtent=-1, **kwargs):
-
-        super(IntervalScrollBarThumb, self).__init__(nodeMap=None, **kwargs)
-
-        self.__upNode = widget.HStretchNode(src=upBmp, endsExtent=endsExtent, minExtent=minExtent)
-        self.__downNode = widget.HStretchNode(src=downBmp, endsExtent=endsExtent, minExtent=minExtent)
-        self.__disabledNode = widget.HStretchNode(src=disabledBmp, endsExtent=endsExtent, minExtent=minExtent)
-
-        self.setNodeMap({
-            "UP": self.__upNode,
-            "DOWN": self.__downNode,
-            "DISABLED": self.__disabledNode
-        })
-        self.visibleid = "UP"
