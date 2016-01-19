8188cf68eedbedae1a780f8131707b0ff35b2c9d
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-19 23:14:41 +0100

    Time_Frame now has its own file/class: The global Time_frame is "Time_Frame.main_time_frame". A Time_Frame can be subscribed to i.e.:
    my_time_frame.subscribe(my_subscriber)
    
    -> my_subscriber must have "update_time_frame(self, interval)" - function.
    Each interval-change in my_time_frame is published to all subscribers
     which means "update_time_frame(...)" is called on each subscriber.
    
    
    Former-commit-id: 8f03cc20c33755c2236d3c9fd4b78c0182f8f0d7

--- Draw.py
+++ Draw.py
@@ -1,11 +1,12 @@
-from libavg import app, avg
-import libavg
-import User
-import Util
-import axis
-import Visualization
-import global_values
 import math
+
+from libavg import app, avg
+
+import libavg
+import Util
+import User
+import Line_Visualization
+import Time_Frame
 
 
 class main_drawer(app.MainDiv):
@@ -30,33 +31,23 @@ class main_drawer(app.MainDiv):
 
     def onInit(self):
         # self.resolution = libavg.app.instance._resolution
-        self.resolution = (libavg.app.instance._resolution[0], libavg.app.instance._resolution[1] - 80)
+        self.resolution = (libavg.app.instance._resolution[0], libavg.app.instance._resolution[1])
         libavg.RectNode(pos=(0, 0), size=self.resolution, fillcolor='ffffff', parent=self, strokewidth=0)
         for userid in range(1, 5):
             user = User.User(userid)
-        self.visualizations.append(Visualization.Visualization(parent=self, size=self.resolution, position=(0, 0)))
+        main_visualization = Line_Visualization.Line_Visualization(parent=self, size=self.resolution, position=(0, 0))
+        self.visualizations.append(main_visualization)
+        Time_Frame.main_time_frame.subscribe(main_visualization)
+
         self.subscribe(avg.Node.MOUSE_WHEEL, self.onMouseWheel)
         app.keyboardmanager.bindKeyDown(keyname='Right', handler=self.shift_forward)
         app.keyboardmanager.bindKeyDown(keyname='Left', handler=self.shift_back)
         app.keyboardmanager.bindKeyDown(keyname='Up', handler=self.zoom_in)
         app.keyboardmanager.bindKeyDown(keyname='Down', handler=self.zoom_out)
 
-
-
-        # axes
-        self.time_axis = axis.TimeAxisNode(size=(self.width - 100, 20), pos=(50, self.height - 40), parent=self,
-                                           data_range=global_values.total_range, unit="ms")
-        self.y_axis = axis.AxisNode(size=(libavg.app.instance._resolution[1] - 80, 30), pos=(25, 25), parent=self,
-                                    vertical=True, data_range=global_values.x_range, unit="cm")
-
     def onFrame(self):
-        if global_values.update_interval_range():
-            for visualization in self.visualizations:
-                visualization.createLine()
+        Time_Frame.main_time_frame.update_interval_range()
 
-            true_range = global_values.total_range[1] - global_values.total_range[0]
-            offset = global_values.get_interval_range()[0]  # self.shift_current * true_range
-            self.time_axis.update(visualization.start * true_range, visualization.end * true_range, offset)
 
     def draw_line(self, p1, p2, color, thickness, last_thickness, opacity):
         return libavg.LineNode(pos1=p1, pos2=p2, color=color, strokewidth=thickness, parent=self)
@@ -69,22 +60,22 @@ class main_drawer(app.MainDiv):
         return [start_points[0], end_points[0], end_points[1], start_points[1]]
 
     def zoom_in(self):
-        global_values.zoom_in_at(0.5)
+        Time_Frame.main_time_frame.zoom_in_at(0.5)
 
     def zoom_out(self):
-        global_values.zoom_out_at(0.5)
+        Time_Frame.main_time_frame.zoom_out_at(0.5)
 
     def shift_back(self):
-        global_values.shift_time(False)
+        Time_Frame.main_time_frame.shift_time(False)
 
     def shift_forward(self):
-        global_values.shift_time(True)
+        Time_Frame.main_time_frame.shift_time(True)
 
     def onMouseWheel(self, event):
         if event.motion.y > 0:
-            global_values.zoom_in_at(event.pos[0] / self.resolution[0])
+            Time_Frame.main_time_frame.zoom_in_at(event.pos[0] / self.resolution[0])
         else:
-            global_values.zoom_out_at(event.pos[0] / self.resolution[0])
+            Time_Frame.main_time_frame.zoom_out_at(event.pos[0] / self.resolution[0])
 
 
 def calculate_line_intersection(p1, p2_selected, p3, thickness1, thickness2_selected, thickness3):
@@ -93,6 +84,7 @@ def calculate_line_intersection(p1, p2_selected, p3, thickness1, thickness2_sele
     thickness3 *= 0.5
     vector_1 = (p2_selected[0] - p1[0], p2_selected[1] - p1[1])
     vector_2 = (p3[0] - p2_selected[0], p3[1] - p2_selected[1])
+
     vector_length_1 = math.sqrt(vector_1[0] * vector_1[0] + vector_1[1] * vector_1[1])
     vector_length_2 = math.sqrt(vector_2[0] * vector_2[0] + vector_2[1] * vector_2[1])
     try:
@@ -114,10 +106,6 @@ def calculate_line_intersection(p1, p2_selected, p3, thickness1, thickness2_sele
 
     intersection_point_1 = Util.line_intersection((left_1, left2_1), (left2_2, left_3))
     intersection_point_2 = Util.line_intersection((right_1, right2_1), (right2_2, right_3))
-    distance_1 = Util.get_length((intersection_point_1[0] - tuple(p2_selected)[0], intersection_point_1[1] - tuple(p2_selected)[1]))
-
-    if (distance_1 > 12):
-        return [left2_2, right2_2]
 
     return [intersection_point_1, intersection_point_2]
 
