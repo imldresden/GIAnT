12098e4e0573e279ee0dc6052268547bae5bd4f7
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-19 23:14:41 +0100

    Time_Frame now has its own file/class: The global Time_frame is "Time_Frame.main_time_frame". A Time_Frame can be subscribed to i.e.:
    my_time_frame.subscribe(my_subscriber)
    
    -> my_subscriber must have "update_time_frame(self, interval)" - function.
    Each interval-change in my_time_frame is published to all subscribers
     which means "update_time_frame(...)" is called on each subscriber.
    
    
    Former-commit-id: 4a7573c9d62b1335594d8a9e0b0b8f397a40c68f

--- global_values.py
+++ global_values.py
@@ -1,92 +1,10 @@
 import colorsys
-import database
-import time
 
 colors_hsv = [[35 / float(360), 0.41, 1], [0, 0.50, 1], [287 / float(360), 0.34, 1], [205 / float(360), 0.33, 1]]
 samples_per_pixel = 0.2
 averaging_count = 500
-total_range = [database.min_time, database.max_time]
-x_range = [database.min_x, database.max_x]
-__interval_range = [total_range[0], total_range[1]]
-__interval_range_last = [total_range[0], total_range[1]]
-interval_range_target = [total_range[0], total_range[1]]    # temporary global
-animation_start_time = -1                                   # temporary global
-__animation_duration = 1
-__zoom_strength = 0.1
 
-
-# updates the current interval to the interpolated value for the animation
-def update_interval_range():
-    global animation_start_time, __animation_duration, interval_range_target, __interval_range, __interval_range_last
-    if animation_start_time == -1:
-        return False
-
-    progress = (time.time() - animation_start_time) / __animation_duration
-    if progress >= 1:
-        __interval_range = list(interval_range_target)
-
-        animation_start_time = -1
-        return True
-    __interval_range[0] = progress * interval_range_target[0] + (1 - progress) * __interval_range_last[0]
-    __interval_range[1] = progress * interval_range_target[1] + (1 - progress) * __interval_range_last[1]
-    return True
-
-
-# gets the interval for the current animation step
-def get_interval_range():
-    global __interval_range
-    update_interval_range()
-    return __interval_range
-
-
-# zooms in around the mouse (sets the target interval, which is later animated using update_interval_range())
-def zoom_in_at(fraction_in_timeframe):
-    global __interval_range, animation_start_time, __interval_range_last, interval_range_target
-    __interval_range_last = list(__interval_range)
-    animation_start_time = time.time()
-    update_interval_range()
-
-    point = interval_range_target[0] + fraction_in_timeframe * (interval_range_target[1] - interval_range_target[0])
-    interval_range_target[0] = point - (point - interval_range_target[0]) * (1 - __zoom_strength)
-    interval_range_target[1] = point + (interval_range_target[1] - point) * (1 - __zoom_strength)
-
-# zooms out around the mouse (sets the target interval, which is later animated using update_interval_range())
-def zoom_out_at(fraction_in_timeframe):
-    global __interval_range, total_range, animation_start_time, __interval_range_last, interval_range_target
-    __interval_range_last = list(__interval_range)
-    animation_start_time = time.time()
-    update_interval_range()
-    point = interval_range_target[0] + fraction_in_timeframe * (interval_range_target[1] - interval_range_target[0])
-    interval_range_target[0] -= (point - interval_range_target[0]) * 1 / ((1 / __zoom_strength) - 1)
-    interval_range_target[1] += (interval_range_target[1] - point) * 1 / ((1 / __zoom_strength) - 1)
-
-    if interval_range_target[0] < total_range[0]:
-        interval_range_target[0] = total_range[0]
-
-    if interval_range_target[1] > total_range[1]:
-        interval_range_target[1] = total_range[1]
-
-
-# shifts the interval (sets the target interval, which is later animated using update_interval_range())
-def shift_time(forwards):
-    global __interval_range, total_range, animation_start_time, __interval_range_last
-    __interval_range_last = list(__interval_range)
-    animation_start_time = time.time()
-    update_interval_range()
-    if forwards:
-        shift_amount = (interval_range_target[1] - interval_range_target[0]) * __zoom_strength
-    else:
-        shift_amount = -(interval_range_target[1] - interval_range_target[0]) * __zoom_strength
-    if interval_range_target[0] + shift_amount < total_range[0]:
-        shift_amount = total_range[0] - interval_range_target[0]
-    if interval_range_target[1] + shift_amount > total_range[1]:
-        shift_amount = total_range[1] - interval_range_target[1]
-
-    interval_range_target[0] += shift_amount
-    interval_range_target[1] += shift_amount
-
-
-def getColorAsHex(index, opacity):
+def get_color_as_hex(index, opacity):
     if index < 0 or index > 3:
         index = 0
         print "color index out of range"
