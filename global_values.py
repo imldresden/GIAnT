f5e338406096cc09e77c195a2aa6ea8be0565e56
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-23 05:21:32 +0100

    refactoring, recoloring
    Former-commit-id: 5c964c8306d187469f071cce698f355bf7de9989

--- global_values.py
+++ global_values.py
@@ -1,11 +1,11 @@
 import database
 
 samples_per_pixel = 0.1
-averaging_count = 456
-user_colors_hsv = [[35 / float(360), 0.41, 1],
-                   [0, 0.50, 1],
-                   [287 / float(360), 0.34, 1],
-                   [205 / float(360), 0.33, 1]]
+averaging_count = 500
+user_colors_hls = [[ 35 / float(360), 0.409, 1], # Yellow
+                   [  0 / float(360), 0.500, 1], # Red
+                   [286 / float(360), 0.567, 1], # Purple
+                   [152 / float(360), 0.568, 1]] # Green
 x_range = [database.min_x, database.max_x]
 COLOR_FOREGROUND = "FFFFFF"
 COLOR_SECONDARY = "333333"
