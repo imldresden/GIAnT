6b2be249990b024e48d13eac97539836de7799da
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-28 19:41:53 +0100

    neutral hsl color can now be calculated: get_user_color_as_hex(-1, opacity)
    Former-commit-id: bfebcf38cd5e3745060af89e681c403e498e97ec

--- Util.py
+++ Util.py
@@ -105,10 +105,13 @@ def normalize_vector(vector):
 
 
 def get_user_color_as_hex(index, opacity):
-    if index < 0 or index > 3:
-        index = 0
-        print "user color index out of range"
-    hls = global_values.user_colors_hls[index]
+    if index == -1:
+        hls = [0, 0, 1]
+    else:
+        if index < 0 or index > 3:
+            index = 0
+            print "user color index out of range"
+        hls = global_values.user_colors_hls[index]
     hls = colorsys.hsv_to_rgb(hls[0], min(1, hls[1] * opacity * opacity * opacity * opacity * 4), hls[2])
     color = (int(hls[0] * 255), int(hls[1] * 255), int(hls[2] * 255))
     color = '%02x%02x%02x' % color
