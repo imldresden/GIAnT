c8e23db7fe2c5524073f9077bf45f57022c9df21
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-29 10:44:20 +0100

    fixed possible crash
    Former-commit-id: 8bff810c9999bfd0bd57f55d62449b6d5544dece

--- User.py
+++ User.py
@@ -25,9 +25,9 @@ class User:
     def get_head_position_averaged(self, index):
         count = min(global_values.averaging_count, len(self.head_positions_integral) - index - 1)
         integral = self.head_positions_integral
-        index = min(max(0, index), len(integral) - 1)
         if count == 0:
             count = 1
+        index = min(max(0, index), len(integral) - count - 1)
 
         head_position = [(integral[index + count][0] - integral[index][0]) / count,
                          (integral[index + count][1] - integral[index][1]) / count,
@@ -38,9 +38,9 @@ class User:
     def get_view_point_averaged(self, index):
         count = min(global_values.averaging_count, len(self.viewpoints_integral) - index - 1)
         integral = self.viewpoints_integral
-        index = min(max(0, index), len(integral) - 1)
         if count == 0:
             count = 1
+        index = min(max(0, index), len(integral) - count - 1)
 
         head_position = [(integral[index + count][0] - integral[index][0]) / count,
                          (integral[index + count][1] - integral[index][1]) / count,
@@ -53,7 +53,7 @@ class User:
     def __init__(self, index):
         self.index = index - 1
 
-        #self.head_positions = database.get_head_positions(index)
+        # self.head_positions = database.get_head_positions(index)
         self.head_positions_integral = database.get_head_positions_integral(index)
         # self.head_times = []
         # for head_position in self.head_positions:
