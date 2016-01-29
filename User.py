eef04f6a0cff6dc016ab92a9c9fdaff3de9b3f09
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-01-29 01:38:28 +0100

    Viewpoints now are calculated, put in the database and visualized Axis-positioning was fixed too some refactoring
    Former-commit-id: 6f807c286c6dd8317f83345e1564ef9c66186c85

--- User.py
+++ User.py
@@ -35,18 +35,31 @@ class User:
                          integral[index + count][3]]
         return head_position
 
+    def get_view_point_averaged(self, index):
+        count = min(global_values.averaging_count, len(self.viewpoints_integral) - index - 1)
+        integral = self.viewpoints_integral
+        index = min(max(0, index), len(integral) - 1)
+        if count == 0:
+            count = 1
+
+        head_position = [(integral[index + count][0] - integral[index][0]) / count,
+                         (integral[index + count][1] - integral[index][1]) / count,
+                         integral[index + count][2]]
+        return head_position
+
     def clearViewpoints(self):
         self.viewpoints = []
 
     def __init__(self, index):
         self.index = index - 1
 
-        self.head_positions = database.get_head_positions(index)
+        #self.head_positions = database.get_head_positions(index)
         self.head_positions_integral = database.get_head_positions_integral(index)
-        self.head_times = []
-        for head_position in self.head_positions:
-            self.head_times.append(head_position[3])
-        self.viewpoints = database.get_view_points(index)
+        # self.head_times = []
+        # for head_position in self.head_positions:
+        #     self.head_times.append(head_position[3])
+
+        self.viewpoints_integral = database.get_view_points_integral(index)
 
         self.touches = database.get_touch_positions(index)
 
