7fb585ced115b6119c292d90f94379b8bf81a912
David Stolze <s5300172@mail.zih.tu-dresden.de>
2016-02-03 03:57:53 +0100

    Video synchronization Video positioning (not quite good yet)
    Former-commit-id: 521ded82eb3b097a356873b51e02264cd9818671

--- Video.py
+++ Video.py
@@ -4,13 +4,15 @@ import Time_Frame
 
 
 class Video:
-    offset = (60 * 5 + 12.5) * 1000
+    offset = (60 * 6 + 8.1) * 1000
 
     def __init__(self, pos, size, parent):
         self.frames = 0
         vid_size = (size[0], size[0] * 9.0 / 16.0)
         if size[0] / size[1] > 16.0 / 9.0:
             vid_size = (size[1] * 16.0 / 9.0, size[1])
+
+        pos = (pos[0] + (size[0] - vid_size[0]) / 2, pos[1] + (size[1] - vid_size[1]) / 2)
 
         self.is_playing = False
         self.videoNode = avg.VideoNode(href="csv/M2U00003.MPG", pos=pos,
@@ -32,7 +34,6 @@ class Video:
     def play_pause(self, play=True):
         self.is_playing = play
         time = int(Time_Frame.main_time_frame.get_interval_range()[1] + self.offset)
-        print time
         self.videoNode.seekToTime(time)
         if play:
             self.videoNode.play()
