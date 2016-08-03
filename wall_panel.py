b86b4debfd93171355e2882f817fb9b834453797
Ulrich von Zadow <uzadow@libavg.de>
2016-08-03 17:36:20 +0200

    Wall and floor coords aligned; draw wall extents on wall pane.

--- wall_panel.py
+++ wall_panel.py
@@ -13,32 +13,46 @@ class WallPanel(vis_panel.VisPanel):
 
         self.__users = session.users
 
-        self.__create_display_borders()
-        self._create_x_axis(data_range=pat_model.x_wall_range, unit="m")
-        self._create_y_axis(data_range=pat_model.y_wall_range, unit="m", hide_rims=True, inverted=True)
+        pos_range = pat_model.pos_range
+        self._create_x_axis(data_range=(pos_range[0][0], pos_range[1][0]), unit="m")
+        self._create_y_axis(data_range=(pos_range[0][1], pos_range[1][1]), unit="m", hide_rims=True, inverted=True)
 
+        # Calculate size of wall in pixels.
+        view_extent = (pos_range[1][0] - pos_range[0][0], pos_range[1][1] - pos_range[0][1])
+        normalized_wall_pos = avg.Point2D((pat_model.x_wall_range[0] - pos_range[0][0]) / view_extent[0],
+                (pat_model.y_wall_range[1] - pos_range[0][1]) / view_extent[1])
+        wall_pos = avg.Point2D(self._data_div.size.x * normalized_wall_pos.x,
+                self._data_div.size.y * (1-normalized_wall_pos.y))
+        wall_size = avg.Point2D(self._data_div.size.x * pat_model.x_wall_range[1] / view_extent[0],
+                self._data_div.size.y * (1-pat_model.y_wall_range[0] / view_extent[1]))
+        avg.RectNode(pos=wall_pos, size=wall_size, color=global_values.COLOR_SECONDARY,
+                parent=self._data_div)
+
+        self.__plot_div = avg.DivNode(pos=wall_pos, size=wall_size, parent=self._data_div)
         self._create_data_div()
+        self.__create_display_borders()
 
         self.__plot_nodes = []
         for user in self.__users:
             color = vis_params.get_user_color(user.userid)
-            node = plots.ScatterPlotNode(size=self._data_div.size, viewportrangemax=pat_model.touch_range,
-                    color=color, parent=self._data_div)
+            node = plots.ScatterPlotNode(size=self.__plot_div.size, viewportrangemax=pat_model.touch_range,
+                    color=color, parent=self.__plot_div)
             self.__plot_nodes.append(node)
 
     def _update_time(self, vis_params):
         self.__show_touches(vis_params.get_time_interval())
 
     def __create_display_borders(self):
+        parent = self.__plot_div
         for i in range(1,3):
-            y = i/3. * self._grid_div.height
-            avg.LineNode(pos1=(0,y), pos2=(self._grid_div.width,y), color=global_values.COLOR_BACKGROUND,
-                    parent=self._grid_div)
+            y = i/3. * parent.height
+            avg.LineNode(pos1=(0, y), pos2=(parent.width, y), color=global_values.COLOR_BACKGROUND,
+                    parent=parent)
 
         for i in range(1, 4):
-            x = i / 4. * self._grid_div.width
-            avg.LineNode(pos1=(x, 0), pos2=(x, self._grid_div.height), color=global_values.COLOR_BACKGROUND,
-                parent=self._grid_div)
+            x = i / 4. * parent.width
+            avg.LineNode(pos1=(x, 0), pos2=(x, parent.height), color=global_values.COLOR_BACKGROUND,
+                parent=parent)
 
     def __show_touches(self, time_interval):
         for i, user in enumerate(self.__users):
