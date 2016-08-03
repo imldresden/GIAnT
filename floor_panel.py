9a0a56893f7e91181b67d54d526517ec2107a6f0
Ulrich von Zadow <uzadow@libavg.de>
2016-08-03 17:46:24 +0200

    Show wall on floor view.

--- floor_panel.py
+++ floor_panel.py
@@ -16,6 +16,7 @@ class FloorPanel(vis_panel.VisPanel):
         pos_range = pat_model.pos_range
         self._create_x_axis(data_range=(pos_range[0][0], pos_range[1][0]), unit="m", top_axis=True)
         self._create_y_axis(data_range=(-0.5,2.5), unit="m", hide_rims=True)
+        self.__create_wall_rect()
 
         self._create_data_div()
 
@@ -26,3 +27,10 @@ class FloorPanel(vis_panel.VisPanel):
         for i, user in enumerate(self.__users):
             pass
 
+    def __create_wall_rect(self):
+        x_min = self._x_axis.value_to_pixel(0)
+        x_max = self._x_axis.value_to_pixel(pat_model.wall_width)
+        y_max = self._y_axis.value_to_pixel(0)
+
+        avg.RectNode(pos=(x_min, y_max-16), size=(x_max - x_min, 16), fillcolor=global_values.COLOR_DARK_GREY,
+                fillopacity=1, parent=self._data_div)
